9c7ec34bd31ceff27f11b25f9c837cbf5db9933a
Nicholas Orlowsky <oldgh@nickorlow.com>
2021-10-16 13:35:03 -0500

    Update Hero.tsx

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
                 flexDirection: "column",
                 alignItems: "center"
             }}>
-                <img alt="github profile" src={"https://avatars.githubusercontent.com/u/85139485?v=4"}
+                <img alt="github profile" src={"https://github.com/account"}
                      style={{height: "30vh", width: "auto"}} className={"pfp"}/>
                 <h1 className={"m-0 font-weight-bold"}>Nicholas Orlowsky</h1>
                 <h4 className={"m-0"}>Software Engineer</h4>
