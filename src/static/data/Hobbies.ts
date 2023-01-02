8e66c60a79f307c7d1237f1ab9fcfea9bd8b0083
Nicholas Orlowsky <nickorlow@nickorlow.com>
2023-01-02 12:58:09 -0600

    Update Hobbies.ts

--- src/static/data/Hobbies.ts
+++ src/static/data/Hobbies.ts
@@ -12,7 +12,7 @@ const RunningHobby: InfoCardProps = {
 
 const Lifting: InfoCardProps = {
     title: "Lifting",
-    description: "Once I was done with cross country, I was so used to working out everyday, I just couldn't stop. I started lifting as a break from my 6 years and 10,000 miles of running and really really liked it.",
+    description: "I started lifting as a break from my 6 years and 10,000 miles of running and really liked it.",
     listTitle: "Personal Records",
     list:["Bench - 270lbs", "Squat - 405lbs", "Deadlift - 385lbs"],
     listClassName: "col-12"
